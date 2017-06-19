856c416e8257c9073b455ed60a56f47ce8e6b378
Joao dos Santos Carmona <jpc@mm.st>
2017-06-19 15:59:20 +0200

    feat: update to koa 2

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
+const minify = require('html-minifier').minify;
 
-var minify = require('html-minifier').minify
-
-module.exports = function (options) {
-  options = options || {}
-  return function* minifyHTML(next) {
-    yield* next
-
-    if (!this.response.is('html')) return
-    var body = this.response.body
-    if (!body) return
-    // too lazy to handle streams
-    if (typeof body.pipe === 'function') return
-    if (Buffer.isBuffer(body)) body = body.toString('utf8')
-    else if (typeof body === 'object') return // wtf programming
-    this.response.body = minify(body, options)
-  }
-}
+module.exports = (options = {}) => {
+	return async (ctx, next) => {
+		await next();
+		if (!ctx.response.is('html')) return;
+		let { body } = ctx.response;
+		if (!body) return;
+		if (typeof body.pipe === 'function') return;
+		if (Buffer.isBuffer(body)) body = body.toString('utf8');
+		else if (typeof body === 'object') return;
+		ctx.response.body = await minify(body, options);
+	};
+};
