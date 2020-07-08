c1b1d54e7385d451b302ae6e6a5081c4059d304a
3imed-jaberi <imed_jebari@hotmail.fr>
2020-07-08 13:04:12 +0200

    setup linter --run --fix 🍭 ..

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const { minify } = require('html-minifier-terser');
 
-module.exports = function koaHtmlMinifier (options) {
-	return async (ctx, next) => {
-		await next();
-		if (
-			!options ||
-			!ctx.response.is('html') ||
-			!ctx.response.body ||
-			typeof ctx.response.body.pipe === 'function'
-		) return;
+module.exports = function koaHtmlMinifier(options) {
+  return async (ctx, next) => {
+    await next();
+    if (
+      !options ||
+      !ctx.response.is('html') ||
+      !ctx.response.body ||
+      typeof ctx.response.body.pipe === 'function'
+    )
+      return;
 
-		if(Buffer.isBuffer(ctx.response.body)) 
-			ctx.response.body = ctx.response.body.toString('utf8');
+    if (Buffer.isBuffer(ctx.response.body))
+      ctx.response.body = ctx.response.body.toString('utf8');
 
-		if (typeof ctx.response.body === 'object') return;
+    if (typeof ctx.response.body === 'object') return;
 
-		ctx.response.body = await minify(ctx.response.body, options);
-	};
+    ctx.response.body = await minify(ctx.response.body, options);
+  };
 };
